8bedcd511e50187585eb4b13877efdaf909bd45a
JamesCheek21 <jamesfrancis80@aol.com>
2019-11-11 16:07:04 +0000

    Add Project

---
+++ QuestProject/Assets/Person/Patrol.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] points;
+    private int destPoints = 0;
+    private NavMeshAgent agent;
+
+    public bool go = false;
+    public float initialDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+        GoToNextPoint();
+
+        initialDelay = Random.Range(2.0f, 12.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!agent.pathPending && agent.remainingDistance < 0.5)
+        {
+            GoToNextPoint();
+        }
+
+        if(!go)
+        {
+            initialDelay -= Time.deltaTime;
+            if (initialDelay <= 0.0f)
+            {
+                go = true;
+                GoToNextPoint();
+            }
+            else return;
+        }
+    }
+    void GoToNextPoint()
+    {
+          if(points.Length == 0)
+            return;
+
+        agent.destination = points[destPoints].position;
+
+        destPoints = (destPoints + 1) % points.Length;
+
+    }
+}
---
+++ QuestProject/Assets/TrafficLights.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLights : MonoBehaviour
+{
+    public List<Transform> trafficlights;
+
+    public GameObject tl1green;
+    public GameObject tl1red;
+
+    public GameObject tl2green;
+    public GameObject tl2red;
+
+    public GameObject tl3green;
+    public GameObject tl3red;
+
+    public float initialDelay = 3.0f;
+    public int state = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trafficlights = new List<Transform>();
+        GameObject[] trafficlight;
+        trafficlight = GameObject.FindGameObjectsWithTag("Traffic Light");
+
+        for(int i = 0; i <= trafficlight.Length; i++)
+        {
+            trafficlights.Add(trafficlight[i].transform);
+        }
+        LightControl();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        initialDelay -= Time.deltaTime;
+        if (initialDelay < 0)
+        {
+            initialDelay = 3.0f;
+            LightControl();
+            if (state == 1)
+                state++;
+            else if (state == 2)
+                state--;
+
+        }
+    }
+    void LightControl()
+    {
+        if (state==1)
+        {
+            tl1green.active = true;
+            tl1red.active = false;
+            tl2green.active = false;
+            tl2red.active = true;
+            tl3green.active = false;
+            tl3red.active = true;
+        }
+        else if(state==2)
+        {
+            tl1green.active = false;
+            tl1red.active = true;
+            tl2green.active = true;
+            tl2red.active = false;
+            tl3green.active = true;
+            tl3red.active = false;
+        }
+    }
+}
