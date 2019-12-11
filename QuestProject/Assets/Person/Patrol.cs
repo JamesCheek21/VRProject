e9004f1a1d2d118513bcc4aa772b20cf21884e8a
JamesCheek21 <jamesfrancis80@aol.com>
2019-12-11 17:58:10 +0000

    finished city

--- QuestProject/Assets/Person/Patrol.cs
+++ QuestProject/Assets/Person/Patrol.cs
@@ -8,6 +8,7 @@ public class Patrol : MonoBehaviour
     public Transform[] points;
     //private int destPoints = 0;
     public NavMeshAgent agent;
+	Animator animator;
 
     public bool go = false;
     public float initialDelay;
@@ -15,6 +16,7 @@ public class Patrol : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator>();
         agent.autoBraking = false;
         int r = Random.Range(0, points.Length);
         agent.SetDestination(points[r].position);
